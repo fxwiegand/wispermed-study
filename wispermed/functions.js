232c7d0171d46d8db0f3f30c3c801593c43c9611
fxwiegand <fxwiegand@wgdnet.de>
2025-02-13 15:52:07 +0100

    Deploy report

--- wispermed/functions.js
+++ wispermed/functions.js
@@ -1,28 +1,6 @@
-function custom_func_1ea4e8b2cbb894a7b4e5d063acf2556a(value) { try { 
-    return `<p style="line-height: normal !important;">${value.replace(/([.!?])\s*(?=[A-Z])/g, "$1<br>")}</p>`;
+function custom_func_5104aff66095d803905934e19ab48064(value) { try { 
+    return `<p style="white-space: pre-wrap; max-width: 50vw; line-height: 1.5;">${value}</p>`;
  } catch (e) { datavzrd.custom_error(e, 'Abstract') }}
-function custom_func_b0dbe029c96fe1e10225c2afa8ba3435(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "Ja") {
-      return trueLabel;
-  } else if (value === "Nein") {
-      return falseLabel;
-  } else {
-      return value;
-  }
- } catch (e) { datavzrd.custom_error(e, 'Klinische Studie (Ja/Nein)') }}
-function custom_func_b0dbe029c96fe1e10225c2afa8ba3435(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "Ja") {
-      return trueLabel;
-  } else if (value === "Nein") {
-      return falseLabel;
-  } else {
-      return value;
-  }
- } catch (e) { datavzrd.custom_error(e, 'Open Data? (Ja/Nein)') }}
 function custom_func_1276c91c4e6dede9cdd6a2c63b649824(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
   const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
@@ -34,6 +12,31 @@ function custom_func_1276c91c4e6dede9cdd6a2c63b649824(value) { try {
       return value;
   }
  } catch (e) { datavzrd.custom_error(e, 'Wurde die Studie im Paper mal in einer Guideline erwähnt? (J/N)') }}
+function custom_func_b0dbe029c96fe1e10225c2afa8ba3435(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "Ja") {
+      return trueLabel;
+  } else if (value === "Nein") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'Klinische Studie (Ja/Nein)') }}
+function custom_func_65340dd852998d1c2b1946611b346347(value) { try { 
+    return `<span data-toggle="tooltip" title="The Oxford Levels of Evidence is a hierarchical system that ranks research studies based on their methodological quality and reliability. It classifies evidence from Level 1 (which includes high-quality randomized controlled trials and systematic reviews) down to Level 5 (which consists of expert opinions and case reports). Rather than using a mathematical formula, the calculation involves evaluating a study's design, execution, consistency, and directness to determine its appropriate level in the hierarchy.">${value}</span>`;
+ } catch (e) { datavzrd.custom_error(e, 'Oxford Evidenzlevel') }}
+function custom_func_b0dbe029c96fe1e10225c2afa8ba3435(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "Ja") {
+      return trueLabel;
+  } else if (value === "Nein") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'Open Data? (Ja/Nein)') }}
 function custom_func_6c7776a30d2dd6f14eb71298f1f5de1b(value) { try { 
     const match = value.match(/(.+?)\s*\(h-index:\s*(\d+)\)/);
     if (!match) return value; // Falls das Format nicht passt, gib den ursprünglichen Wert zurück
@@ -102,9 +105,6 @@ function custom_func_6c7776a30d2dd6f14eb71298f1f5de1b(value) { try {
         ">${hIndex}</span>
     `;
  } catch (e) { datavzrd.custom_error(e, 'First Author (Name + h-index)') }}
-function custom_func_65340dd852998d1c2b1946611b346347(value) { try { 
-    return `<span data-toggle="tooltip" title="The Oxford Levels of Evidence is a hierarchical system that ranks research studies based on their methodological quality and reliability. It classifies evidence from Level 1 (which includes high-quality randomized controlled trials and systematic reviews) down to Level 5 (which consists of expert opinions and case reports). Rather than using a mathematical formula, the calculation involves evaluating a study's design, execution, consistency, and directness to determine its appropriate level in the hierarchy.">${value}</span>`;
- } catch (e) { datavzrd.custom_error(e, 'Oxford Evidenzlevel') }}
 function custom_func_49004fb0ceae291d236987ba0866b418(value, row) { try { 
     // Generate data for this cell, having access to the value and any other values from the same row.
     let values = value.split(',').map(Number);
